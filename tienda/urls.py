4b74a17edd2fe9eec4855cccc9b23a23d5174986
Federico <federicomtaborda@gmail.com>
2022-08-08 16:20:59 -0300

    add singnals login registro logout

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,7 +3,7 @@ from tienda import views
 from tienda.views import VerProductos
 
 urlpatterns = [
-    path('cargar/', views.cargar_imagen, name="cargar"),
+    path('cargar/', views.cargar_producto, name="cargar"),
     path('<int:producto_id>/ver/', views.ver_producto, name="ver"),  
     path('verproductos/', VerProductos.as_view(), name="verproductos"),
 ]
