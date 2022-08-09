bd5f7aecdda15c61ca675bd6d103b3a2b3d29ce7
Federico <federicomtaborda@gmail.com>
2022-08-09 18:00:07 -0300

    add signals

--- usuarios/views_registro.py
+++ usuarios/views_registro.py
@@ -1,14 +1,16 @@
 from django.shortcuts import redirect, render
-#from usuarios.forms import CreateUserForm
+from usuarios.forms import CreateUserForm
 
 def pagina_registro(request):
     params={}
-    # form=CreateUserForm()
-    # params["form"]=form
+    form=CreateUserForm()
+    params["form"]=form
 
-    # if request.method =="POST":
-    #     form=CreateUserForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         return redirect("login")
+    if request.method =="POST":
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            print("error")
     return render(request, "usuarios/registro.html", params)
